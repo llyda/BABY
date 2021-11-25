735a229f78cf7cbdbdc0c42ebcc9937cfe1bdb26
Marco Zausch <jamaza@posteo.de>
2021-11-25 18:04:49 +0100

    newest update

--- BABY/cleaner.py
+++ BABY/cleaner.py
@@ -2,11 +2,9 @@ import re
 import emoji
 import string
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 import json
 stop_words = set(stopwords.words('english'))
-lemma = WordNetLemmatizer()
+
 
 
 
@@ -14,16 +12,25 @@ def cleaner(tweet_list):
     '''combines different cleansing methods'''
     # clean tweets
     clean_tweets = [tweet_cleaner(x) for x in tweet_list]
-    # remove empty strings
-    no_empty_strings = [i for i in clean_tweets if i]
     # unicode
-    uni_tweets = [unicode_maker(x) for x in no_empty_strings]
+    uni_tweets = [unicode_maker(x) for x in clean_tweets]
     # remove numbers and punctuation
     number_free = [number_punct(x) for x in uni_tweets]
-    # json as output of the function
-    jsonString = json.dumps(number_free)
+    # tweets longer than two words
+    more_than_two = [x for x in number_free if (len(x.split())>2)]
+    # remove duplicates
+    clean_set = set(more_than_two)
+    # remove empty strings
+    no_empty_strings = [i for i in clean_set if i]
+    # strip whitespace in front and end
+    stripped = [x.strip() for x in no_empty_strings]
+    # fix double spaces
+    one_space = [re.sub(' +', ' ', x) for x in stripped]
+    # make one long string with full stop and linebreak
+    one_long_string = ".\n".join(one_space)
 
-    return jsonString
+    return one_long_string
+
 
 
 
@@ -47,6 +54,7 @@ def tweet_cleaner(tweet):
 
 
 def unicode_maker(text):
+    '''get's rid of unicode'''
     # creating a unicode string
     text_encode = text.encode(encoding="ascii", errors="ignore")
     text_decode = text_encode.decode()
@@ -57,6 +65,7 @@ def unicode_maker(text):
 
 
 def number_punct(sentence):
+    '''removes numbers, punctuation, uppercase'''
     # remove numbers
     number_free = ''.join(word for word in sentence if not word.isdigit())
     # remove punctuation
@@ -66,32 +75,3 @@ def number_punct(sentence):
     upper_free = punctuation_free.lower()
 
     return upper_free
-
-
-
-
-
-'''we don't use tokens or lemmas for now'''
-
-def text_to_token(sentence):
-    # remove numbers
-    number_free = ''.join(word for word in sentence if not word.isdigit())
-    # remove punctuation
-    punctuation_free = "".join(
-        [i for i in number_free if i not in string.punctuation])
-    # lower case
-    upper_free = punctuation_free.lower()
-    # tokenize
-    word_tokens = word_tokenize(upper_free)
-    # remove stopwords
-    important_words = [w for w in word_tokens if not w in stop_words]
-
-    return important_words
-
-
-
-
-def get_lemm(l):
-    lemm = [lemma.lemmatize(word) for word in l]
-
-    return lemm
